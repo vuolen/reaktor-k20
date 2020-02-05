51af7524ff5d664f1809899b82e4292d0b73fce2
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2020-02-05 10:23:33 +0200

    Revert "Parses multiple fields"
    This reverts commit 4276ce1a3cc0f68ad920d3a82f85fb537565770d.

--- test/reaktor_k20/parser_test.clj
+++ test/reaktor_k20/parser_test.clj
@@ -17,9 +17,3 @@
 (is (= (parse "Package: \t  WhitespacePackage \t\t  ")
        {:Package "WhitespacePackage"})
     "parse should ignore whitespace around the value")
-
-(is (= (parse "Package: PackageValue\nDescription: desc\r\nVersion: vers")
-       {:Package "PackageValue"
-        :Description "desc"
-        :Version "vers"})
-    "parse should handle multiple fields on their own lines")
