9cefb4ae28e90da8cb23490705467cf2b8c001ba
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2020-02-09 15:59:53 +0200

    Added more htmlgen tests

--- src/reaktor_k20/htmlgen.clj
+++ src/reaktor_k20/htmlgen.clj
@@ -1,1 +1,8 @@
 (ns reaktor-k20.htmlgen)
+
+(defn generate
+  "Takes a package and returns a hiccup html"
+  [package]
+  [:div {:class "package"}
+   (when-let [name (:Package package)]
+     [:div {:class "package-name"} name])])
--- test/reaktor_k20/htmlgen_test.clj
+++ test/reaktor_k20/htmlgen_test.clj
@@ -3,4 +3,21 @@
    [reaktor-k20.htmlgen :refer :all]
    [clojure.test :refer :all]])
 
-(deftest htmlgen-test-suite)
+(deftest htmlgen-test-suite
+  (let [top-div (generate {:Package "PackageName"})]
+    (is (= (first top-div)
+           :div)
+        "generate should return a top level div")
+    (is (= (:class (second top-div))
+           "package")
+        "generate should return a top level div with the class \"package\"")
+    (let [package-div (nth top-div 2)]
+      (is (= (first package-div)
+             :div)
+          "given a package name, generate should have a child div")
+      (is (= (:class (second package-div))
+             "package-name")
+          "given a package name, generate should have a child div with the class  \"package-name\"")
+      (is (= (nth package-div 2)
+             "PackageName")
+          "given a package name, generate should have a child div with the package name as content"))))
