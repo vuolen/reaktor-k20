e7f05d0dc45a956d7c5612b74f9fd9fdcbc60f94
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2020-02-11 14:39:03 +0200

    htmlgen supports paragraphs

--- src/reaktor_k20/htmlgen.clj
+++ src/reaktor_k20/htmlgen.clj
@@ -1,8 +1,34 @@
-(ns reaktor-k20.htmlgen)
+(ns reaktor-k20.htmlgen
+  [require
+   [clojure.string :as str]])
+
+(defn description-paragraph-line
+  "Lines starting with a single space are part of a paragraph. Successive lines of this form will be word-wrapped when displayed. The leading space will usually be stripped off. The line must contain at least one non-whitespace character."
+  [html string]
+  (let [trimmed (str/trim string)]
+    (if (vector? (last html))
+      (update-in html
+                 [(- (count html) 1) 2]
+                 str " " trimmed)
+      (conj html [:p nil trimmed]))))
+
+(defn generate-description
+  "Takes a description as a string and generates a hiccup html form"
+  [description]
+  (reduce (fn [html string]
+            (cond
+              (re-find #"^ [^ .].*" string) (description-paragraph-line html string)
+              :else (conj html string)))
+          []
+          (str/split-lines description)))
 
 (defn generate
-  "Takes a package and returns a hiccup html"
+  "Takes a map and returns a hiccup html"
   [package]
-  [:div {:class "package"}
-   (when-let [name (:Package package)]
-     [:div {:class "package-name"} name])])
+  (concat [:div {:class "package"}]
+          (remove nil?
+                  [(when-let [name (:Package package)]
+                     [:div {:class "package-name"} name])
+                   (when-let [description (:Description package)]
+                     (concat [:div {:class "description"}]
+                             (generate-description description)))])))
