d80c4e6e3789ae19e4bc8572482e0b0d43157d69
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2020-02-12 00:06:52 +0200

    Started rewriting htmlgen with simpler tests and code

--- src/reaktor_k20/htmlgen.clj
+++ src/reaktor_k20/htmlgen.clj
@@ -2,33 +2,28 @@
   [require
    [clojure.string :as str]])
 
-(defn description-paragraph-line
-  "Lines starting with a single space are part of a paragraph. Successive lines of this form will be word-wrapped when displayed. The leading space will usually be stripped off. The line must contain at least one non-whitespace character."
-  [html string]
-  (let [trimmed (str/trim string)]
-    (if (vector? (last html))
-      (update-in html
-                 [(- (count html) 1) 2]
-                 str " " trimmed)
-      (conj html [:p nil trimmed]))))
+;; (defn description-paragraph-line
+;;   "Lines starting with a single space are part of a paragraph. Successive lines of this form will be word-wrapped when displayed. The leading space will usually be stripped off. The line must contain at least one non-whitespace character."
+;;   [html string]
+;;   (let [trimmed (str/trim string)]
+;;     (if (= (first (last html))
+;;            :p)
+;;       (update-in html
+;;                  [(- (count html) 1) 2]
+;;                  str " " trimmed)
+;;       (conj html [:p nil trimmed]))))
 
 (defn generate-description
   "Takes a description as a string and generates a hiccup html form"
   [description]
-  (reduce (fn [html string]
-            (cond
-              (re-find #"^ [^ .].*" string) (description-paragraph-line html string)
-              :else (conj html string)))
-          []
-          (str/split-lines description)))
+  [:div {:class "description"}
+   (nth (str/split-lines description) 0)])
 
 (defn generate
-  "Takes a map and returns a hiccup html"
+  "Takes a map and returns a hiccup html form"
   [package]
-  (concat [:div {:class "package"}]
-          (remove nil?
-                  [(when-let [name (:Package package)]
-                     [:div {:class "package-name"} name])
-                   (when-let [description (:Description package)]
-                     (concat [:div {:class "description"}]
-                             (generate-description description)))])))
+  [:div {:class "package"}
+   (when-let [name (:Package package)]
+     [:div {:class "name"} name])
+   (when-let [description (:Description package)]
+     (generate-description description))])
--- test/reaktor_k20/htmlgen_test.clj
+++ test/reaktor_k20/htmlgen_test.clj
@@ -3,32 +3,52 @@
    [reaktor-k20.htmlgen :refer :all]
    [clojure.test :refer :all]])
 
-(deftest htmlgen-test-suite
-  (let [top-div (generate {:Package "PackageName"})]
-    (is (= (first top-div)
+(def test-package1 {:Package "TestPackage"
+                    :Description "First line\n These lines\n are part\n of the same paragraph\n .\n  This line is preceded by a blank line and displayed verbatim"
+                    :Depends ["package1" "package2"]
+                    :Reverse-Depends ["package3" "package4"]})
+
+(def test-package2 {:Package "TestPackage2"
+                    :Description "This is package 2"
+                    :Depends ["packageA" "packageB"]
+                    :Reverse-Depends ["packageC" "packageD"]})
+
+(deftest generate-test-suite
+  (let [result1 (generate test-package1)
+        result2 (generate test-package2)]
+    (is (= (first result1)
            :div)
-        "generate should return a top level div")
-    (is (= (:class (second top-div))
+        "generate should return a div")
+    (is (= (:class (second result1))
            "package")
-        "generate should return a top level div with the class \"package\"")
-    (let [package-div (nth top-div 2)]
-      (is (= (first package-div)
-             :div)
-          "given only a package name, generate should have a child div")
-      (is (= (:class (second package-div))
-             "package-name")
-          "given only a package name, generate should have a child div with the class  \"package-name\"")
-      (is (= (nth package-div 2)
-             "PackageName")
-          "given only a package name, generate should have a child div with the package name as content")))
-  (let [top-div (generate {:Description "First line\n These lines\n are part\n of the same paragraph\n .\n This line is preceded by a blank line"})
-        description-div (nth top-div 2)]
-    (is (= (first description-div)
+        "generate should return a div with the class \"package\"")
+    (is (= (first (nth result1 2))
            :div)
-        "given only a description, generate should have a child div")
-    (is (= (:class (second description-div))
+        "given a package name, generate should have a div as the first child")
+    (is (= (:class (second (nth result1 2)))
+           "name")
+        "given a package name, generate should have a first child with the class \"name\"")
+    (is (= (get-in result1 [2 2])
+           "TestPackage")
+        "given a package named TestPackage, generate should have a first child containing that name")
+    (is (= (get-in result2 [2 2])
+           "TestPackage2")
+        "given a package named TestPackage2, generate should have a first child containing that name")
+    (is (= (first (nth result1 3))
+           :div)
+        "given a package description, generate should have a div as the second child")
+    (is (= (:class (second (nth result1 3)))
            "description")
-        "given only a description, generate should have a child div with the class \"description\"")
-    (is (= (nth description-div 3)
-           [:p nil "These lines are part of the same paragraph"])
-        "generate should handle paragraph lines in the description")))
+        "given a package description, generate should have a second child with the class \"description\""))
+  (is (nil? (nth (generate {:Description "desc"}) 2))
+      "given a package with no name, generate should have a first child of nil")
+  (is (nil? (nth (generate {:Package "TestPackage"}) 3))
+      "given a package with no description, generate should have a second child of nil")
+
+  (is (= (get-in (generate {:Description "Synopsis\n Next line"}) [3 2])
+         "Synopsis")
+      "given a description with the first line of \"Synopsis\", generate should have \"Synopsis\" as the first child of the description div")
+  (is (= (get-in (generate {:Description "Synopsis2\n Next line"}) [3 2])
+         "Synopsis2")
+      "given a description with the first line of \"Synopsis2\", generate should have \"Synopsis2\" as the first child of the description div"))
+
